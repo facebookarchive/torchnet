47749b0439bfa1176aaf8ab057c60c1e8724b40f
Laurens van der Maaten <lvdmaaten@gmail.com>
2016-06-27 19:08:31 -0400

    add mnist example

---
+++ example/mnist.lua
@@ -0,0 +1,81 @@
+--[[
+Copyright (c) 2016-present, Facebook, Inc.
+All rights reserved.
+This source code is licensed under the BSD-style license found in the
+LICENSE file in the root directory of this source tree. An additional grant
+of patent rights can be found in the PATENTS file in the same directory.
+]]--
+
+-- load torchnet:
+local tnt = require 'torchnet'
+
+-- function that sets of dataset iterator:
+local function getIterator(mode)
+   return tnt.ParallelDatasetIterator{
+      nthread = 1,
+      init    = function() require 'torchnet' end,
+      closure = function()
+
+         -- load MNIST dataset:
+         local mnist = require 'mnist'
+         local dataset = mnist[mode .. 'dataset']()
+         dataset.data = dataset.data:reshape(dataset.data:size(1),
+            dataset.data:size(2) * dataset.data:size(3)):double()
+
+         -- return batches of data:
+         return tnt.BatchDataset{
+            batchsize = 128,
+            dataset = tnt.ListDataset{  -- replace this by your own dataset
+               list = torch.range(1, dataset.data:size(1)):long(),
+               load = function(idx)
+                  return {
+                     input  = dataset.data[idx],
+                     target = torch.LongTensor{dataset.label[idx] + 1},
+                  }  -- sample contains input and target
+               end,
+            }
+         }
+      end,
+   }
+end
+
+-- set up logistic regressor:
+local net = nn.Sequential():add(nn.Linear(784,10))
+local criterion = nn.CrossEntropyCriterion()
+
+-- set up training engine:
+local engine = tnt.SGDEngine()
+local meter  = tnt.AverageValueMeter()
+local clerr  = tnt.ClassErrorMeter{topk = {1}}
+engine.hooks.onStartEpoch = function(state)
+   meter:reset()
+   clerr:reset()
+end
+engine.hooks.onForwardCriterion = function(state)
+   meter:add(state.criterion.output)
+   clerr:add(state.network.output, state.sample.target)
+   if state.training then
+      print(string.format('avg. loss: %2.4f; avg. error: %2.4f',
+         meter:value(), clerr:value{k = 1}))
+   end
+end
+
+-- train the model:
+engine:train{
+   network   = net,
+   iterator  = getIterator('train'),
+   criterion = criterion,
+   lr        = 0.2,
+   maxepoch  = 5,
+}
+
+-- measure test loss and error:
+meter:reset()
+clerr:reset()
+engine:test{
+   network   = net,
+   iterator  = getIterator('test'),
+   criterion = criterion,
+}
+print(string.format('test loss: %2.4f; test error: %2.4f',
+   meter:value(), clerr:value{k = 1}))
