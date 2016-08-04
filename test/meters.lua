5f3b26a77f446e44ec910cbd77d2a1f3cdeb6f1e
Max Gordon <max@gforge.se>
2016-08-04 07:23:06 +0200

    Added mAPMeter Fixed indentation

--- test/meters.lua
+++ test/meters.lua
@@ -39,56 +39,56 @@ function test.ClassErrorMeter()
 end
 
 function test.ConfusionMeter()
-	local mtr = tnt.ConfusionMeter{k = 3}
+   local mtr = tnt.ConfusionMeter{k = 3}
 
-	-- The max value is the one that is correct
-	local output = torch.Tensor({{.8,0.1,0.1},{10,11,10},{0.2,0.2,.3}})
-	local target = torch.Tensor({1,2,3})
-	mtr:add(output, target)
-	local conf_mtrx = mtr:value()
+   -- The max value is the one that is correct
+   local output = torch.Tensor({{.8,0.1,0.1},{10,11,10},{0.2,0.2,.3}})
+   local target = torch.Tensor({1,2,3})
+   mtr:add(output, target)
+   local conf_mtrx = mtr:value()
 
-	tester:eq(conf_mtrx:sum(), 3, "All should be correct")
-	tester:eq(torch.diag(conf_mtrx):sum(), 3, "All should be correct")
+   tester:eq(conf_mtrx:sum(), 3, "All should be correct")
+   tester:eq(torch.diag(conf_mtrx):sum(), 3, "All should be correct")
 
-	target[1] = 2
-	target[2] = 1
-	target[3] = 1
-	mtr:add(output, target)
+   target[1] = 2
+   target[2] = 1
+   target[3] = 1
+   mtr:add(output, target)
 
-	tester:eq(conf_mtrx:sum(), 6, "Six tests should give six values")
-	tester:eq(torch.diag(conf_mtrx):sum(), 3, "Shouldn't have changed since all new values were false")
-	tester:eq(conf_mtrx[1]:sum(), 3, "All top have gotten one guess")
-	tester:eq(conf_mtrx[2]:sum(), 2, "Two first at the 2nd row have a guess")
-	tester:eq(conf_mtrx[2][3], 0, "The last one should be empty")
-	tester:eq(conf_mtrx[3]:sum(), 1, "Bottom row has only the first test correct")
-	tester:eq(conf_mtrx[3][3], 1, "Bottom row has only the first test correct")
+   tester:eq(conf_mtrx:sum(), 6, "Six tests should give six values")
+   tester:eq(torch.diag(conf_mtrx):sum(), 3, "Shouldn't have changed since all new values were false")
+   tester:eq(conf_mtrx[1]:sum(), 3, "All top have gotten one guess")
+   tester:eq(conf_mtrx[2]:sum(), 2, "Two first at the 2nd row have a guess")
+   tester:eq(conf_mtrx[2][3], 0, "The last one should be empty")
+   tester:eq(conf_mtrx[3]:sum(), 1, "Bottom row has only the first test correct")
+   tester:eq(conf_mtrx[3][3], 1, "Bottom row has only the first test correct")
 
-	-- Test normalized version
-	mtr = tnt.ConfusionMeter{k = 4, normalized=true}
-	output = torch.Tensor({
-		{.8,0.1,0.1,0},
-		{10,11,10,0},
-		{0.2,0.2,.3,0},
-		{0,0,0,1}
-	})
+   -- Test normalized version
+   mtr = tnt.ConfusionMeter{k = 4, normalized=true}
+   output = torch.Tensor({
+      {.8,0.1,0.1,0},
+      {10,11,10,0},
+      {0.2,0.2,.3,0},
+      {0,0,0,1}
+   })
 
-	target = torch.Tensor({1,2,3,4})
-	mtr:add(output, target)
-	conf_mtrx = mtr:value()
+   target = torch.Tensor({1,2,3,4})
+   mtr:add(output, target)
+   conf_mtrx = mtr:value()
 
-	tester:eq(conf_mtrx:sum(), output:size(2), "All should be correct")
-	tester:eq(torch.diag(conf_mtrx):sum(), output:size(2), "All should be correct")
+   tester:eq(conf_mtrx:sum(), output:size(2), "All should be correct")
+   tester:eq(torch.diag(conf_mtrx):sum(), output:size(2), "All should be correct")
 
-	target[1] = 2
-	target[2] = 1
-	target[3] = 1
-	mtr:add(output, target)
-	conf_mtrx = mtr:value()
+   target[1] = 2
+   target[2] = 1
+   target[3] = 1
+   mtr:add(output, target)
+   conf_mtrx = mtr:value()
 
-	tester:eq(conf_mtrx:sum(), output:size(2), "The noramlization should sum all values to 1")
-	for i=1,output:size(2) do
-		tester:eq(conf_mtrx[i]:sum(), 1, "Row no " .. i .. " fails to sum to one in normalized mode")
-	end
+   tester:eq(conf_mtrx:sum(), output:size(2), "The noramlization should sum all values to 1")
+   for i=1,output:size(2) do
+      tester:eq(conf_mtrx[i]:sum(), 1, "Row no " .. i .. " fails to sum to one in normalized mode")
+   end
 end
 
 function test.AUCMeter()
@@ -114,6 +114,72 @@ end
 
 
 function test.APMeter()
+   local mtr = tnt.APMeter()
+
+   local target = torch.Tensor{0,1,0,1}
+   local output = torch.Tensor{.1,.2,.3,4}
+   mtr:add(output, target)
+
+   local ap = mtr:value()
+   tester:eq(ap[1], (1*1 + 0*1/2 + 2*1/3 + 0*1/4)/2)
+
+   mtr:reset()
+
+   target = torch.Tensor{0,1,0,1}
+   output = torch.Tensor{4,3,2,1}
+   mtr:add(output, target)
+
+   ap = mtr:value()
+   tester:eq(ap[1], (0*1 + 1*1/2 + 0*1/3 + 2*1/4)/2)
+
+   mtr:reset()
+   target = torch.Tensor{0,1,0,1}
+   output = torch.Tensor{1,4,2,3}
+   mtr:add(output, target)
+
+   ap = mtr:value()
+   tester:eq(ap[1], (1*1 + 2*1/2 + 0*1/3 + 0*1/4)/2)
+
+   mtr:reset()
+   target = torch.Tensor{0,0,0,0}
+   output = torch.Tensor{1,4,2,3}
+   mtr:add(output, target)
+
+   ap = mtr:value()
+   tester:eq(ap[1], 0)
+
+   mtr:reset()
+   target = torch.Tensor{1,1,0}
+   output = torch.Tensor{3,1,2}
+   mtr:add(output, target)
+
+   ap = mtr:value()
+   tester:eq(ap[1], (1*1 + 0*1/2 + 2*1/3)/2)
+
+   -- Test multiple K:s
+   mtr:reset()
+   target = torch.Tensor{
+      {0,1,0,1},
+      {0,1,0,1}
+   }:transpose(1,2)
+   output = torch.Tensor{
+      {.1,.2,.3,4},
+      {4,3,2,1}
+   }:transpose(1,2)
+   mtr:add(output, target)
+
+   ap = mtr:value()
+   tester:eq(
+      ap,
+      torch.DoubleTensor{
+         (1*1 + 0*1/2 + 2*1/3 + 0*1/4)/2,
+         (0*1 + 1*1/2 + 0*1/3 + 2*1/4)/2
+      }
+   )
+end
+
+
+function test.mAPMeter()
    local mtr = tnt.mAPMeter()
 
    local target = torch.Tensor{0,1,0,1}
@@ -123,38 +189,26 @@ function test.APMeter()
    local ap = mtr:value()
    tester:eq(ap, (1*1 + 0*1/2 + 2*1/3 + 0*1/4)/2)
 
+   -- Test multiple K:s
    mtr:reset()
-
-   local target = torch.Tensor{0,1,0,1}
-   local output = torch.Tensor{4,3,2,1}
+   target = torch.Tensor{
+      {0,1,0,1},
+      {0,1,0,1}
+   }:transpose(1,2)
+   output = torch.Tensor{
+      {.1,.2,.3,4},
+      {4,3,2,1}
+   }:transpose(1,2)
    mtr:add(output, target)
 
-   local ap = mtr:value()
-   tester:eq(ap, (0*1 + 1*1/2 + 0*1/3 + 2*1/4)/2)
-
-   mtr:reset()
-   local target = torch.Tensor{0,1,0,1}
-   local output = torch.Tensor{1,4,2,3}
-   mtr:add(output, target)
-
-   local ap = mtr:value()
-   tester:eq(ap, (1*1 + 2*1/2 + 0*1/3 + 0*1/4)/2)
-
-   mtr:reset()
-   local target = torch.Tensor{0,0,0,0}
-   local output = torch.Tensor{1,4,2,3}
-   mtr:add(output, target)
-
-   local ap = mtr:value()
-   tester:eq(ap, 0)
-
-   mtr:reset()
-   local target = torch.Tensor{1,1,0}
-   local output = torch.Tensor{3,1,2}
-   mtr:add(output, target)
-
-   local ap = mtr:value()
-   tester:eq(ap, (1*1 + 0*1/2 + 2*1/3)/2)
+   ap = mtr:value()
+   tester:eq(
+      ap,
+      torch.DoubleTensor{
+         (1*1 + 0*1/2 + 2*1/3 + 0*1/4)/2,
+         (0*1 + 1*1/2 + 0*1/3 + 2*1/4)/2
+      }:mean()
+   )
 end
 
 return function(_tester_)
