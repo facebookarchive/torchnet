5d4a2a2ec4a785e38101c26e654c46fa2e7f8f46
Max Gordon <max@gforge.se>
2016-08-04 07:47:58 +0200

    Added initial MultilabelConfusionMeter tests

--- test/meters.lua
+++ test/meters.lua
@@ -91,6 +91,67 @@ function test.ConfusionMeter()
    end
 end
 
+function test.MultilabelConfusionMeter()
+   local mtr = tnt.MultiLabelConfusionMeter{k = 3, normalized=false}
+
+   -- The max value is the one that is correct
+   local output = torch.Tensor({{.8,0.1,0.1},{10,11,10},{0.2,0.2,.3}})
+   local target = torch.LongTensor({1,2,3})
+   local one_hot = torch.zeros(output:size())
+   one_hot:scatter(2, target:view(-1,1), 1)
+   mtr:add(output, one_hot)
+   local conf_mtrx = mtr:value()
+
+   tester:eq(conf_mtrx, torch.eye(3), "All should be correct")
+
+   target[1] = 2
+   target[2] = 1
+   target[3] = 1
+   one_hot = torch.zeros(output:size())
+   one_hot:scatter(2, target:view(-1,1), 1)
+   mtr:add(output, one_hot)
+   conf_mtrx = mtr:value()
+
+   tester:eq(conf_mtrx:sum(), 6, "Six tests should give six values")
+   tester:eq(torch.diag(conf_mtrx):sum(), 3, "Shouldn't have changed since all new values were false")
+   tester:eq(conf_mtrx[1]:sum(), 3, "All top have gotten one guess")
+   tester:eq(conf_mtrx[2]:sum(), 2, "Two first at the 2nd row have a guess")
+   tester:eq(conf_mtrx[2][3], 0, "The last one should be empty")
+   tester:eq(conf_mtrx[3]:sum(), 1, "Bottom row has only the first test correct")
+   tester:eq(conf_mtrx[3][3], 1, "Bottom row has only the first test correct")
+
+   -- Test normalized version
+   mtr = tnt.MultiLabelConfusionMeter{k = 4, normalized=true}
+   output = torch.Tensor({
+      {.8,0.1,0.1,0},
+      {10,11,10,0},
+      {0.2,0.2,.3,0},
+      {0,0,0,1}
+   })
+
+   target = torch.LongTensor({1,2,3,4})
+   one_hot = torch.zeros(output:size())
+   one_hot:scatter(2, target:view(-1,1), 1)
+   mtr:add(output, one_hot)
+   conf_mtrx = mtr:value()
+
+   tester:eq(conf_mtrx:sum(), output:size(2), "All should be correct", 10^-3)
+   tester:eq(torch.diag(conf_mtrx):sum(), output:size(2), "All should be correct", 10^-3)
+
+   target[1] = 2
+   target[2] = 1
+   target[3] = 1
+   one_hot = torch.zeros(output:size())
+   one_hot:scatter(2, target:view(-1,1), 1)
+   mtr:add(output, one_hot)
+   conf_mtrx = mtr:value()
+
+   tester:eq(conf_mtrx:sum(), output:size(2), "The noramlization should sum all values to 1", 10^-3)
+   for i=1,output:size(2) do
+      tester:eq(conf_mtrx[i]:sum(), 1, "Row no " .. i .. " fails to sum to one in normalized mode", 10^-3)
+   end
+end
+
 function test.AUCMeter()
    local mtr = tnt.AUCMeter()
 
